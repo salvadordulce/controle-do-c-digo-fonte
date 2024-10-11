36e36ed5b40719af04eef371342c93b79c72af84
Dulce Maria <dm9100546@gmail.com>
2024-10-10 21:43:33 -0300

    adicionando-prisma

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -1,17 +1,23 @@
 const pool = require("../config/database"); // Importanto conexão do banco de dados
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 // Função que retorna todos os itens armazenados
 async function listItems() {
-  const buscarItems = await pool.query('SELECT * FROM items');
+  const buscarItems = await prisma.items.findMany();
 
-  return buscarItems[0];
+  return buscarItems;
 };
 
 // Função que cria um novo item e o adiciona à lista
 async function createItem(name) {
-  const newItem = await pool.query(`INSERT INTO items (name) VALUES ('${name}')`)
+  const newItem = await prisma.items.create({
+    data: {
+      name: name
+    }
+  });
 
-  return newItem[0];
+  return newItem;
 };
 
 
